7025858061670f479230894bf08b8e8feabf0883
Vidhu Kant Sharma <vidhukant@protonmail.ch>
2021-12-17 01:42:21 +0530

    First commit

---
+++ phrases.py
@@ -0,0 +1,32 @@
+ON_READY_MESSAGE = "Logged in as {client_username}"
+PING_MESSAGE = "Maa chuda behenchod"
+
+KICK_BAN = {
+    "MissingRequiredArgument": "Please specify a member for the action."
+    , "MissingPermissions": "You don't have the permission to do so."
+    , "MemberNotFound": "Member does not exist."
+    , "CommandInvokeError": "Failed to kick user."
+
+    , "member_kicked": "Member {member} has been kicked for reason \"{reason}\""
+    , "member_banned": "Member {member} has been banned for reason \"{reason}\""
+    , "member_unbanned": "User {member} has been unbanned."
+    , "unban_member_notfound": "Banned member {member} not found"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+WELCOME_MESSAGE = "प्रिय {username} हमारे सर्वर की तीरथ यात्रा में शामिल होने के लिए हम भक्तगण तुम्हारा स: हृदय स्वागत करते हैं।\n 🙌🙌👏👏🙏🙏🙌🙌👏👏🙏🙏🐄🐒🐅\n गाना कॉपी करने के लाए स्टार दबाएं।\n नियम कृपया #रूल्स में पाएं।। "
+GOODBYE_MESSAGE = "{username} has left."
