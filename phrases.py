1b935d5cb54f9d2d3e2a59287e4b7c89fde3c3b6
Vidhu Kant Sharma <vidhukant@protonmail.ch>
2021-12-17 02:20:21 +0530

    Om translated this to Hindi LMAOO

--- phrases.py
+++ phrases.py
@@ -2,15 +2,19 @@ ON_READY_MESSAGE = "Logged in as {client_username}"
 PING_MESSAGE = "Maa chuda behenchod"
 
 KICK_BAN = {
-    "MissingRequiredArgument": "Please specify a member for the action."
-    , "MissingPermissions": "You don't have the permission to do so."
-    , "MemberNotFound": "Member does not exist."
-    , "CommandInvokeError": "Failed to kick user."
+    "MissingRequiredArgument": "कृपया किसी भक्त का नाम मेंशन करें।"
+    , "MissingPermissions": "आपकी ये एक्शन लेने की औकात नही है।"
+    , "MemberNotFound": "बेटीचोद! ऐसा कोई भक्त नहीं है। "
+    , "CommandInvokeError": "यह भक्त किक नहीं हो पाए। "
 
-    , "member_kicked": "Member {member} has been kicked for reason \"{reason}\""
-    , "member_banned": "Member {member} has been banned for reason \"{reason}\""
-    , "member_unbanned": "User {member} has been unbanned."
-    , "unban_member_notfound": "Banned member {member} not found"
+    , "member_kicked":"{member} को प्रस्तुत कारण से किक किया गया है =>
+{कारण"}
+    , "member_banned": "{member} को प्रस्तुत कारण से बैन किया गया है =>
+{कारण}
+
+    , "member_unbanned": "{member} भक्त का सत्संग को फरसे दरबार में आने की अनुमति प्रदान कर दी गई है
+"
+    , "unban_member_notfound": "{member} भक्त को बैन हो चुके हैं।"
 }
 
 
@@ -29,4 +33,6 @@ KICK_BAN = {
 
 
 WELCOME_MESSAGE = "प्रिय {username} हमारे सर्वर की तीरथ यात्रा में शामिल होने के लिए हम भक्तगण तुम्हारा स: हृदय स्वागत करते हैं।\n 🙌🙌👏👏🙏🙏🙌🙌👏👏🙏🙏🐄🐒🐅\n गाना कॉपी करने के लाए स्टार दबाएं।\n नियम कृपया #रूल्स में पाएं।। "
-GOODBYE_MESSAGE = "{username} has left."
+GOODBYE_MESSAGE = "{username}नास्तिक हो चुके हैं।
+।
+ "
