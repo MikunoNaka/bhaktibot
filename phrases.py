e16aa6707d6c6949796d2e99564f05ac486053d4
Vidhu Kant Sharma <vidhukant@protonmail.ch>
2021-12-17 09:20:05 +0530

    Added status, prefix and stuff

--- phrases.py
+++ phrases.py
@@ -1,5 +1,8 @@
 ON_READY_MESSAGE = "Logged in as {client_username}"
 PING_MESSAGE = "Maa chuda behenchod"
+COMMAND_NOT_FOUND_MESSAGE = "I don't know that command?!"
+BOT_STATUS = "Bhakti me leen..."
+INVALID_USERNAME_MESSAGE = "Invalid user. correct format is USER#1234"
 
 KICK_BAN = {
     # error
@@ -15,7 +18,10 @@ KICK_BAN = {
     , "member_unbanned": "{member} भक्त का सत्संग को फरसे दरबार में आने की अनुमति प्रदान कर दी गई है"
 }
 
-INVALID_USERNAME_MESSAGE = "Invalid user. correct format is USER#1234"
+CLEAR = {
+    "MissingPermissions": "You don't have the permissions to manage messages in this channel."
+    , "ValueError": "Invalid arguments. Please specify number or messages (or leave blank to clear whole channel)"
+}
 
 WELCOME_MESSAGE = "प्रिय {username} हमारे सर्वर की तीरथ यात्रा में शामिल होने के लिए हम भक्तगण तुम्हारा स: हृदय स्वागत करते हैं।\n 🙌🙌👏👏🙏🙏🙌🙌👏👏🙏🙏🐄🐒🐅\n गाना कॉपी करने के लाए स्टार दबाएं।\n नियम कृपया #रूल्स में पाएं।। "
 GOODBYE_MESSAGE = "{username}नास्तिक हो चुके हैं।"
